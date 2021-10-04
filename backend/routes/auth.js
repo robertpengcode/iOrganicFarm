3dc3521f5ba89b141f3499dce0e3b54e87e62e9b
RobertPeng <jenpopeng@gmail.com>
2021-10-04 07:47:19 -0400

    restructuring

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -36,7 +36,7 @@ router.post("/login", async (req, res) => {
   const validPass = await bcrypt.compare(req.body.password, user.password);
   if (!validPass) return res.status(400).send("Password is wrong");
   
-  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);  
+  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET,{ expiresIn: '1h' });  
   res.header('auth-token', token).send(token);
 
 });
